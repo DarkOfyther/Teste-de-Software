ca17bf245c8cef2049f1da72d3887973a7bd8536
DarkOfyther <l.goncalvess5002@gmail.com>
2024-08-22 10:36:48 -0300

    test, testando e-mail e senha.

--- testes-e2e-cypress-main/cypress/e2e/formularioLogin.cy.js
+++ testes-e2e-cypress-main/cypress/e2e/formularioLogin.cy.js
@@ -10,9 +10,22 @@ describe('Formulário de login', () => { // contexto do nosso teste
       cy.getByData('email-input').type('neilton@alura');
       cy.getByData('senha-input').type('123456');
       cy.getByData('botao-enviar').click();
-
       cy.getByData('mensagem-erro').should('exist').and('have.text', 'O email digitado é inválido');
   
+    })
+    it('Não deve permitir o campo e-mail em branco', () => {
+        cy.getByData('botao-login').click();
+        cy.getByData('senha-input').type('123456');
+        cy.getByData('botao-enviar').click();
+        cy.getByData('mensagem-erro').should('exist').and('have.text', 'O campo email é obrigatório');
+      });
+
+      it('Não deve permitir o campo senha em branco', () => {
+        cy.getByData('botao-login').click();
+        cy.getByData('email-input').type('neilton@alura');
+        cy.getByData('botao-enviar').click();
+        cy.getByData('mensagem-erro').should('exist').and('have.text', 'O campo de senha é obrigatório');
+      });
     });
-  });
+  
   
