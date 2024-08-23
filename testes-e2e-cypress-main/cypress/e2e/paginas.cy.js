099dd53f767a89ad14470bf3ef3782d295655190
DarkOfyther <l.goncalvess5002@gmail.com>
2024-08-23 11:35:20 -0300

    test, testando as opções do menu, validando se estão direcionando para URL correta.

--- testes-e2e-cypress-main/cypress/e2e/paginas.cy.js
+++ testes-e2e-cypress-main/cypress/e2e/paginas.cy.js
@@ -5,25 +5,35 @@ describe('Testando múltiplas páginas', () => {
     cy.getByData('email-input').type('neilton@alura.com')
     cy.getByData('senha-input').type('123456')
     cy.getByData('botao-enviar').click()
+    
     })
 
-
+   
     it('Deve conseguir acessar a página de cartões', () => {
+        
     cy.getByData('app-home').find('a').eq(1).click()
+    cy.location('pathname').should('eq','/home/cartoes')
     cy.getByData('titulo-cartoes').should('exist').and('have.text','Meus cartões')
     })
 
     it('Deve conseguir acessar o investimento', () => {
         cy.getByData('app-home').find('a').eq(3).click()
+        cy.location('pathname').should('eq','/home/investimentos')
         cy.getByData('titulo-investimento').should('exist').and('have.text','Investimentos')
     })
     it('Deve conseguir acessar o serviços', () => {
         cy.getByData('app-home').find('a').eq(2).click()
+        cy.location('pathname').should('eq','/home/servicos')
         cy.getByData('titulo-servico').should('exist').and('have.descendants','h5','img')
     })
     it('Deve conseguir acessar o inicio', () => {
         cy.getByData('app-home').find('a').eq(0).click()
+        cy.location('pathname').should('eq','/home')
         cy.getByData('titulo-inicio').should('exist').and('have.text', 'Nova Transação')
     })
+
+    
+
+    
 
     })
